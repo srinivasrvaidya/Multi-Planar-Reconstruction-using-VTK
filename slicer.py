0acc0d40fc16413514d0fc5778ea884f543276e2
srinivasrvaidya <srinivas.r.vaidya@gmail.com>
2014-11-25 23:21:38 +0530

    set height/depth of std planes and now we have 2 render windows

--- slicer.py
+++ slicer.py
@@ -49,6 +49,50 @@ center = [x0 + xSpacing * 0.5 * (xMin + xMax),
 print "Center: ", center
 print "(xMin, xMax, yMin, yMax, zMin, zMax)", xMin, xMax, yMin, yMax, zMin, zMax          
           
+# ---------------------------------------------------------------
+         
+axial = vtk.vtkMatrix4x4()
+axial.DeepCopy((1, 0, 0, center[0],
+                0, 1, 0, center[1],
+                0, 0, 1, center[2],
+                0, 0, 0, 1))
+
+coronal = vtk.vtkMatrix4x4()  # x axis
+coronal.DeepCopy((1, 0, 0, center[0],
+                  0, 0, 1, center[1],
+                  0,-1, 0, center[2],
+                  0, 0, 0, 1))
+
+sagittal = vtk.vtkMatrix4x4()
+sagittal.DeepCopy((0, 0,-1, center[0],
+                   1, 0, 0, center[1],
+                   0,-1, 0, center[2],
+                   0, 0, 0, 1))          
+          
+#-------------
+
+# Extract a slice in the desired orientation
+reslice_axial = vtk.vtkImageReslice()
+reslice_axial.SetInputConnection(reader.GetOutputPort())
+reslice_axial.SetOutputDimensionality(2)
+reslice_axial.SetResliceAxes(axial)
+reslice_axial.SetInterpolationModeToLinear()
+
+# Extract a slice in the desired orientation
+reslice_coronal = vtk.vtkImageReslice()
+reslice_coronal.SetInputConnection(reader.GetOutputPort())
+reslice_coronal.SetOutputDimensionality(2)
+reslice_coronal.SetResliceAxes(coronal)
+reslice_coronal.SetInterpolationModeToLinear()
+
+# Extract a slice in the desired orientation
+reslice_sagittal = vtk.vtkImageReslice()
+reslice_sagittal.SetInputConnection(reader.GetOutputPort())
+reslice_sagittal.SetOutputDimensionality(2)
+reslice_sagittal.SetResliceAxes(sagittal)
+reslice_sagittal.SetInterpolationModeToLinear()
+          
+          
 # Create a greyscale lookup table
 table = vtk.vtkLookupTable()
 table.SetRange(0, 2000) # image intensity range
@@ -56,6 +100,37 @@ table.SetValueRange(0.0, 1.0) # from black to white
 table.SetSaturationRange(0.0, 0)
 table.SetRampToLinear()
 table.Build()
+
+
+# Map the image through the lookup table
+color_axial = vtk.vtkImageMapToColors()
+color_axial.SetLookupTable(table)
+color_axial.SetInputConnection(reslice_axial.GetOutputPort())
+
+# Display the image
+actor_axial = vtk.vtkImageActor()
+actor_axial.SetInput(color_axial.GetOutput())
+
+
+# Map the image through the lookup table
+color_coronal = vtk.vtkImageMapToColors()
+color_coronal.SetLookupTable(table)
+color_coronal.SetInputConnection(reslice_coronal.GetOutputPort())
+
+# Display the image
+actor_coronal = vtk.vtkImageActor()
+actor_coronal.SetInput(color_coronal.GetOutput())
+
+# Map the image through the lookup table
+color_sagittal = vtk.vtkImageMapToColors()
+color_sagittal.SetLookupTable(table)
+color_sagittal.SetInputConnection(reslice_sagittal.GetOutputPort())
+
+# Display the image
+actor_sagittal = vtk.vtkImageActor()
+actor_sagittal.SetInput(color_sagittal.GetOutput())
+
+
 
 #-----------------------------------------------------------------
 
@@ -100,7 +175,7 @@ def computeMPR(endPoints):
 	for i in range(0, len(endPoints)-1):	
 		distance[i] = findDistanceBetweenTwoPoints(endPoints[i], endPoints[i+1])	
 		angle[i] = findAngleBetweenTwoPoints(endPoints[i], endPoints[i+1])
-		print "D: ",distance[i],"A: ", angle[i]
+	#	print "D: ",distance[i],"A: ", angle[i]
 
 	temp = -20
 
@@ -151,10 +226,81 @@ def computeMPR(endPoints):
 
 # --- multi-view ---
 rw = vtk.vtkRenderWindow()
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetInteractorStyle(MyInteractorStyle())
-iren.SetRenderWindow(rw)
     
+# Set up the interaction
+interactorStyle = vtk.vtkInteractorStyleImage()
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetInteractorStyle(interactorStyle)
+rw.SetInteractor(interactor)
+
+# --------- another window() ------------
+mpr_rw = vtk.vtkRenderWindow()    
+mpr_iren = vtk.vtkRenderWindowInteractor()
+mpr_iren.SetInteractorStyle(MyInteractorStyle())
+mpr_iren.SetRenderWindow(mpr_rw)
+    
+
+# Create callbacks for slicing the image
+actions = {}
+actions["Slicing"] = 0
+mode = 4
+
+def ButtonCallback(obj, event):
+	
+    if event == "LeftButtonPressEvent":
+        actions["Slicing"] = 1
+    else:
+        actions["Slicing"] = 0
+
+	x, y  = obj.GetInteractor().GetEventPosition()
+	
+	global mode
+	if x < 300 and y < 300:
+		mode = 0 #"SAGITTAL"
+	elif x < 600 and y < 300:
+		mode = 1 #"AXIAL"
+	elif x > 300 and y > 300:
+		mode = 3 #"CORONAL"
+	else:
+		mode = 4
+			
+def MouseMoveCallback(obj, event):
+    (lastX, lastY) = interactor.GetLastEventPosition()
+    (mouseX, mouseY) = interactor.GetEventPosition()
+    if actions["Slicing"] == 1:
+		deltaY = mouseY - lastY
+	
+		if mode ==  3: 		#"CORONAL":
+			reslice_coronal.Update()
+			sliceSpacing = reslice_coronal.GetOutput().GetSpacing()[2]
+			matrix = reslice_coronal.GetResliceAxes()			
+		elif mode == 1: 	#"AXIAL":
+			reslice_axial.Update()
+			sliceSpacing = reslice_axial.GetOutput().GetSpacing()[2]
+			matrix = reslice_axial.GetResliceAxes()
+		elif mode == 0: 	#"SAGITTAL":
+			reslice_sagittal.Update()
+			sliceSpacing = reslice_sagittal.GetOutput().GetSpacing()[2]
+			matrix = reslice_sagittal.GetResliceAxes()	
+		else:
+		#	print " -- 3d model clicked -- "
+			return;
+			
+		# move the center point that we are slicing through
+		center = matrix.MultiplyPoint((0, 0, sliceSpacing*deltaY, 1))
+		matrix.SetElement(0, 3, center[0])
+		matrix.SetElement(1, 3, center[1])
+		matrix.SetElement(2, 3, center[2])
+		rw.Render()        
+		rw.Render()        
+    else:
+        interactorStyle.OnMouseMove()
+
+
+interactorStyle.AddObserver("MouseMoveEvent", MouseMoveCallback)
+interactorStyle.AddObserver("LeftButtonPressEvent", ButtonCallback)
+interactorStyle.AddObserver("LeftButtonReleaseEvent", ButtonCallback)    
+        
 # Define viewport ranges
 xmins=[0, .5 , 0, .5]
 xmaxs=[0.5, 1, 0.5, 1]
@@ -186,41 +332,47 @@ for i in range(0, 4):
 	ren[i] = vtk.vtkRenderer()
 	ren[i].SetViewport(xmins[i],ymins[i],xmaxs[i],ymaxs[i])
 
+renderer = vtk.vtkRenderer()
 
 for i in range(0, numberOfSlices):
-	ren[1].AddActor2D(actor[i])
+	renderer.AddActor2D(actor[i])            
 
-'''
+mpr_rw.AddRenderer(renderer)	
+'''	
 points =[[20,30,47],[30,20,47],[40,30,47]]
 angle = [90,0,-90]
 extentLength = [[64,94],[64,94],[64,94]]
 setLocation = [0,0,0]
 
 computeMPR(points, angle, extentLength, setLocation)
-
-ren[0].AddActor2D(actor[0])
 '''
 
-'''
-ren[0].AddViewProp(slicer2.volume)
-camera = ren[0].GetActiveCamera()
+
+
+ren[2].AddActor2D(actor[0])
+ren[2].AddViewProp(slicer2.volume)
+camera = ren[2].GetActiveCamera()
 c = slicer2.volume.GetCenter()
 camera.SetFocalPoint(c[0], c[1], c[2])
 camera.SetPosition(c[0] + 400, c[1], c[2])
 camera.SetViewUp(0, 0, -1)
-'''
 
 
-ren[2].AddActor2D(actor[1])
-ren[3].AddActor2D(actor[2])
+ren[1].AddActor2D(actor_axial)
+ren[3].AddActor2D(actor_coronal)
+ren[0].AddActor2D(actor_sagittal)
 
 for i in range(0,4):
 	rw.AddRenderer(ren[i])
 
 rw.SetSize(600, 600)
 rw.Render()
-iren.Initialize()
-iren.Start()
+mpr_rw.Render()
+
+#iren.Initialize()
+#iren.Start() 
+
+interactor.Start()
 
 #  renderer 0: BOTTOM LEFT
 #  renderer 1: BOTTOM RIGHT
